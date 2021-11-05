d01e49311e2eead1d24f0dbc4de5ec272b5c86d8
HenryDiaz1202 <henrydiaz1202@gmail.com>
2021-11-04 20:42:06 -0600

    creando y modificando la clase, llamando las funciones y pasando los parámetros

--- app/src/main/java/com/henry/mvp/interactor/OperationInteractor.java
+++ app/src/main/java/com/henry/mvp/interactor/OperationInteractor.java
@@ -1,4 +1,42 @@
 package com.henry.mvp.interactor;
 
-public class OperationInteractor {
+import com.henry.mvp.interfaces.OperationInteractors;
+import com.henry.mvp.interfaces.OperationPresenter;
+
+public class OperationInteractor implements OperationInteractors {
+
+    private int result;
+    private OperationPresenter presenter;
+
+    public OperationInteractor(OperationPresenter operationPresenter) {
+        this.presenter = operationPresenter;
+    }
+
+    @Override
+    public void add(int num1, int num2) {
+        result =  num1 + num2;
+        presenter.showResult(Integer.toString(result));
+    }
+
+    @Override
+    public void subtract(int num1, int num2) {
+        result = num1 - num2;
+        presenter.showResult(Integer.toString(result));
+    }
+
+    @Override
+    public void multiply(int num1, int num2) {
+        result = num1 * num2;
+        presenter.showResult(Integer.toString(result));
+    }
+
+    @Override
+    public void divide(int num1, int num2) {
+        if (num2 == 0) {
+            result = num1 / num2;
+            presenter.showResult(Integer.toString(result));
+        } else {
+            presenter.invalidOperation();
+        }
+    }
 }
--- app/src/main/java/com/henry/mvp/interfaces/OperationInteractors.java
+++ app/src/main/java/com/henry/mvp/interfaces/OperationInteractors.java
@@ -1,4 +1,8 @@
 package com.henry.mvp.interfaces;
 
 public interface OperationInteractors {
+    void add(int num1, int num2);
+    void subtract(int num1, int num2);
+    void multiply(int num1, int num2);
+    void divide(int num1, int num2);
 }
